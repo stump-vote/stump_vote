4813267d4898f710f69a413915751d2a44c5bd3b
Wes Galbraith <galbwe92@gmail.com>
2020-03-15 19:02:22 -0600

    Update api prod build to serve static content through nginx

--- backend/api/stump_backend/stump_backend/settings.py
+++ backend/api/stump_backend/stump_backend/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR,
+                           os.path.pardir,
+                           "staticfiles")
 
 # Localhost React server
 CORS_ORIGIN_WHITELIST = (
