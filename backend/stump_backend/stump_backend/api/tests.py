7a4aed1c212184a0668c03ca090ad8179bf4e0ac
Carlos Perez <perez@doorstep.com>
2020-03-25 12:22:25 -0600

    Add TransactionTestCase support

--- backend/stump_backend/stump_backend/api/tests.py
+++ backend/stump_backend/stump_backend/api/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.test import TransactionTestCase
+from rest_framework import status
 from json import loads
 from .models import Sample
 
 # Create your tests here.
 
 
-class SampleApiTestCase(TestCase):
+class SampleApiTestCase(TransactionTestCase):
 
     def setUp(self):
         super().setUp()
@@ -15,16 +17,28 @@ class SampleApiTestCase(TestCase):
         s1 = Sample.objects.create(title='Title 1', description='Description 1')
         _ = Sample.objects.create(title='Title 2', description='Description 2')
 
-        # list
+        # list view
         url = reverse('sample-list')
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
         response_json = loads(response.content)
         self.assertEquals(response_json, [{'id': 1, 'title': 'Title 1', 'description': 'Description 1'}, {'id': 2, 'title': 'Title 2', 'description': 'Description 2'}])
 
-        # detail
+        # detail view
         url = reverse('sample-detail', kwargs={'pk': s1.pk})
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
         response_json = loads(response.content)
-        self.assertEquals(response_json, {'id': 1, 'title': 'Title 1', 'description': 'Description 1'})
+        self.assertEquals(response_json, {'id': s1.pk, 'title': s1.title, 'description': s1.description})
+
+        # Add item
+        url = reverse('sample-list')
+        data = {
+            'title': 'Title 3',
+            'description': 'Description 3',
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        s3 = Sample.objects.last()
+        self.assertEquals(s3.title, data['title'])
+        self.assertEquals(s3.description, data['description'])
